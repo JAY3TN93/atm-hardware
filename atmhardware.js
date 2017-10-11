b7d2502697442266fac911d61aa5a4debff98cea
Tim Gabets <tim@gabets.ru>
2017-10-11 15:39:19 +0300

    v0.1.4	init()

--- atmhardware.js
+++ atmhardware.js
@@ -1,4 +1,59 @@
 function ATMHardwareService(){
+  
+  this.devices = {};
+
+  this.init = function(){
+    [
+      'Card Capture Bin',
+      'Cash Handler Reject Bin',
+      'Deposit Bin',
+      'Receipt Paper',
+      'Journal Paper',
+      'Night Safe',
+      'Cassettes 1',
+      'Cassettes 2',
+      'Cassettes 3',
+      'Cassettes 4',
+      'Statement Paper',
+      'Statement Ribbon',
+    ].forEach(device => {
+      this.devices[device] = {state: 'Not configured'};
+    });
+  };
+
+  this.getDeviceSupplyStatus = function(name){
+    var device = this.devices[name];
+
+    if(device){
+      return device.state;
+    }
+  };
+
+  this.getSuppliesStatus = function(){
+    var status = '';
+
+    [
+      'Reserved',
+      'Reserved',
+      'Not used',
+      'Card Capture Bin',
+    ].forEach(device => {
+      switch(device){
+        case 'Reserved':
+        case 'Not used':
+          status += 0;
+          break;
+
+        default: 
+          status += this.getDeviceSupplyStatus(device);
+          break;
+      }
+    });
+
+    return status;
+  };
+
+
   this.getReleaseNumber = function(){
     return '030300';
   }
--- spec/ATMHardwareSpec.js
+++ spec/ATMHardwareSpec.js
@@ -6,6 +6,41 @@ describe('ATMHardwareService', function(){
     hw = new ATMHardwareService();
   });
 
+  describe('init()', function(){
+    it('should get init devices', function(){
+      expect(hw.devices).toEqual({});
+      hw.init();
+      var devices = {
+        'Card Capture Bin': { state: 'Not configured' },
+        'Cash Handler Reject Bin': { state: 'Not configured' },
+        'Deposit Bin': { state: 'Not configured' },
+        'Receipt Paper': { state: 'Not configured' },
+        'Journal Paper': { state: 'Not configured' },
+        'Night Safe': { state: 'Not configured' },
+        'Cassettes 1': { state: 'Not configured' },
+        'Cassettes 2': { state: 'Not configured' },
+        'Cassettes 3': { state: 'Not configured' },
+        'Cassettes 4': { state: 'Not configured' },
+        'Statement Paper': { state: 'Not configured' },
+        'Statement Ribbon': { state: 'Not configured'}
+      }
+      expect(hw.devices).toEqual(devices);
+    });
+  });  
+
+  describe('getDeviceSupplyStatus()', function(){
+    it('should get Card Capture Bin deive status', function(){
+      //expect(hw.getDeviceSupplyStatus('Card Capture Bin')).toEqual('Not configured');
+    });
+  });
+
+
+  describe('getSuppliesStatus()', function(){
+    it('should get supply status string', function(){
+      //expect(hw.getSuppliesStatus()).toEqual('00011111001000011130011');
+    });
+  });
+
   describe('getReleaseNumber()', function(){
     it('should get release number', function(){
       expect(hw.getReleaseNumber()).toEqual('030300');
